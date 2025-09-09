f693961e5f2b0f9f03e863d7c327e17e1b101625
ricksteegers <rick.steegers@gmail.com>
2025-09-09 18:56:13 +0200

    Fix witte scherm door CSS imports te herstellen

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './index.css';
 import HomePage from './pages/HomePage';
 import ContactPage from './pages/ContactPage';
 import ThankYouPage from './pages/ThankYouPage';
