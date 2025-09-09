35d56290bf4239f6a5ca4b28f2b154bc45b47241
ricksteegers <rick.steegers@gmail.com>
2025-09-09 19:25:13 +0200

    Updated index.html

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ContactPage from './pages/ContactPage';
-import ThankYouPage from './pages/ThankYouPage';
-import AboutPage from './pages/AboutPage';
 
 function App() {
   return (
@@ -11,8 +9,6 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/contact" element={<ContactPage />} />
-        <Route path="/bedankt" element={<ThankYouPage />} />
-        <Route path="/over-ons" element={<AboutPage />} />
       </Routes>
     </Router>
   );
