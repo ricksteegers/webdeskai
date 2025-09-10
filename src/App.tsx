a317f8e0369b4fdca00b825453b4eadf12c7b059
ricksteegers <rick.steegers@gmail.com>
2025-09-10 12:53:34 +0200

    Restore React ThankYouPage component and remove static HTML

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ContactPage from './pages/ContactPage';
 import AboutPage from './pages/AboutPage';
+import ThankYouPage from './pages/ThankYouPage';
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/contact" element={<ContactPage />} />
         <Route path="/over-ons" element={<AboutPage />} />
+        <Route path="/bedankt" element={<ThankYouPage />} />
+        <Route path="/thankyou" element={<ThankYouPage />} />
       </Routes>
     </Router>
   );
