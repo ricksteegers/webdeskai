47ae61a36e1bb24fc808f7a2f4504d16708c09ec
ricksteegers <rick.steegers@gmail.com>
2025-09-11 13:36:30 +0200

    Remove selected SVG transition element

--- src/components/ProductHighlight.tsx
+++ src/components/ProductHighlight.tsx
@@ -280,12 +280,6 @@ const ProductHighlight: React.FC = () => {
         </div>
       </div>
 
-      {/* Smooth transition to next section */}
-      <div className="absolute bottom-0 left-0 right-0 -mb-1 z-10">
-        <svg className="w-full h-20 fill-current text-gray-50" viewBox="0 0 1200 120" preserveAspectRatio="none">
-          <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0H0Z" fill="rgb(249 250 251)"></path>
-        </svg>
-      </div>
     </section>
   );
 };
