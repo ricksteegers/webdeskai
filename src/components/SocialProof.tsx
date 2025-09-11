78c20f359476259f6fbfcdc6e0378511c7072c73
ricksteegers <rick.steegers@gmail.com>
2025-09-11 13:27:07 +0200

    Remove rotation from SVG path

--- src/components/SocialProof.tsx
+++ src/components/SocialProof.tsx
@@ -71,7 +71,7 @@ const SocialProof: React.FC = () => {
   return (
     <section className="py-20 bg-gradient-to-br from-gray-50 to-white relative overflow-hidden">
       {/* Rotated transition from ProblemSolution */}
-      <div className="absolute top-0 left-0 right-0 -mt-1 z-10 transform rotate-180">
+      <div className="absolute top-0 left-0 right-0 -mt-1 z-10">
         <svg className="w-full h-24 fill-current text-gray-50" viewBox="0 0 1200 120" preserveAspectRatio="none">
           <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0H0Z" fill="white"></path>
           <path d="M0,120C150,20 350,120 600,70C850,20 1050,120 1200,70V0H0V120Z" fill="white" opacity=".4"></path>
